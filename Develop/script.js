fb6bab40aa17be431cba2ed09a1894dafec1bb6c
SafteyDav3 <talktomedw@gmail.com>
2022-03-20 18:03:05 -0500

    First vars | first return function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,83 @@
 // Assignment code here
 
+// ↓↓↓ Arrays to be concatenated | user input ↓↓↓
+var alphaBig = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var alphaSmall = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?"];
+var passLength = "";
+
+// →→→ Array to receive other arrays to generate password from ←←←
+var passwordAry = [];
+
+// →→→ Empty string to hold generated password ←←←
+var generatedPassword = "";
+
+// ↓↓↓ User input / prompts ↓↓↓
+
+var getPassLength = function () {
+  var userPassLength = window.prompt(
+    "Welcome to RandPass!\nPlease enter a number between:\n8-128"
+  );
+  return (passLength = userPassLength);
+};
+
+getPassLength();
+console.log(passLength);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +88,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
