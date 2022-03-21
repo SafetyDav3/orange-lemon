f9c2d9b6b9a5fe4fce8dd3098cedd497c2a6f8fc
SafteyDav3 <talktomedw@gmail.com>
2022-03-20 22:08:46 -0500

    Working cant clear text area on restart

--- Develop/script.js
+++ Develop/script.js
@@ -62,82 +62,88 @@ var specialCharsList = ["!", "@", "#", "$", "%", "&", "*", "?"];
 
 // →→→ User input storage ←←←
 var passLength = null;
-var useUpper = false;
-var useNumbers = false;
-var useSpecialChars = false;
 
 // →→→ Array to receive other arrays to generate password from ←←←
 var passwordAry = [];
 var generatedPasswordItems = [];
+var password = "";
 
 // →→→ Empty string to hold generated password ←←←
 var generatedPassword = "";
 
-// ↓↓↓ User input / prompts ↓↓↓
-// →→→ Getting desired password length ←←←
-var getPassLength = function () {
-  var userPassLength = window.prompt(
-    "Welcome to RandPass!\nPlease enter a number between:\n8-128"
-  );
-  if (userPassLength >= 8 && userPassLength <= 128) {
-    return (passLength = userPassLength);
-  } else {
-    window.alert("Must be between 8-128 characters long.");
-    getPassLength();
-  }
-};
-
-// →→→ Getting uppercase designation ←←←
-var getUpperUse = function () {
-  var userUpper = confirm(
-    "Would you like to use uppercase characters in your password??"
-  );
-  if (userUpper == true) {
-    passwordAry = passwordAry.concat(alphaBig);
-  }
-};
-
-// →→→ Get number designation ←←←
-var getNumberUse = function () {
-  var userNumber = confirm("Would you like to use numbers in your password?");
-  if (userNumber == true) {
-    passwordAry = passwordAry.concat(numList);
-  }
-};
-
-var getSpecCharUse = function () {
-  var userSpecChar = confirm(
-    "Would you like to use special characters in your password?"
-  );
-  if (userSpecChar == true) {
-    passwordAry = passwordAry.concat(specialCharsList);
-  }
-};
-// ↑↑↑ User input complete ↑↑↑
-
-// ↓↓↓ Password generation ↓↓↓
-
 var generatePassword = function () {
-  passwordAry = passwordAry.concat(alphaSmall);
-  for (let i = 1; i <= passLength; i++) {
-    generatedPasswordItems.push(
-      passwordAry[Math.floor(Math.random() * passwordAry.length)]
-    );
-  }
-};
+  // ↓↓↓ User input / prompts ↓↓↓
 
-// ↓↓↓ Calling functions ↓↓↓
-getPassLength();
-console.log(passLength);
-getUpperUse();
-console.log(passwordAry);
-getNumberUse();
-console.log(passwordAry);
-getSpecCharUse();
-console.log(passwordAry);
-generatePassword();
-console.log(generatedPasswordItems);
-console.log(generatedPasswordItems.join(""));
+  // →→→ Getting desired password length ←←←
+  var getPassLength = function () {
+    var userPassLength = window.prompt(
+      "Welcome to RandPass!\nPlease enter a number between:\n8-128"
+    );
+    if (userPassLength >= 8 && userPassLength <= 128) {
+      return (passLength = userPassLength);
+    } else {
+      window.alert("Must be between 8-128 characters long.");
+      getPassLength();
+    }
+  };
+
+  // →→→ Getting uppercase designation ←←←
+  var getUpperUse = function () {
+    var userUpper = confirm(
+      "Would you like to use uppercase characters in your password??"
+    );
+    if (userUpper == true) {
+      passwordAry = passwordAry.concat(alphaBig);
+    }
+  };
+
+  // →→→ Get number designation ←←←
+  var getNumberUse = function () {
+    var userNumber = confirm("Would you like to use numbers in your password?");
+    if (userNumber == true) {
+      passwordAry = passwordAry.concat(numList);
+    }
+  };
+
+  var getSpecCharUse = function () {
+    var userSpecChar = confirm(
+      "Would you like to use special characters in your password?"
+    );
+    if (userSpecChar == true) {
+      passwordAry = passwordAry.concat(specialCharsList);
+    }
+  };
+  // ↑↑↑ User input complete ↑↑↑
+
+  // ↓↓↓ Password generation ↓↓↓
+
+  var generatePass = function () {
+    passwordAry = passwordAry.concat(alphaSmall);
+    for (let i = 1; i <= passLength; i++) {
+      generatedPasswordItems.push(
+        passwordAry[Math.floor(Math.random() * passwordAry.length)]
+      );
+    }
+  };
+
+  // ↓↓↓ Calling functions ↓↓↓
+  getPassLength();
+  console.log(passLength);
+  getUpperUse();
+  console.log(passwordAry);
+  getNumberUse();
+  console.log(passwordAry);
+  getSpecCharUse();
+  console.log(passwordAry);
+  generatePass();
+  console.log(generatedPasswordItems);
+  // ↓↓↓ This is the password! ↓↓↓
+  console.log(generatedPasswordItems.join(""));
+
+  password = generatedPasswordItems.join("");
+
+  return password;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -145,6 +151,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
