eef6d361c9cba0c9094317688c5d59b26ed6a0cc
SafteyDav3 <talktomedw@gmail.com>
2022-03-20 19:19:03 -0500

    Second functions figured out

--- Develop/script.js
+++ Develop/script.js
@@ -59,7 +59,12 @@ var alphaSmall = [
 ];
 var numList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?"];
-var passLength = "";
+
+// →→→ User input storage ←←←
+var passLength = null;
+var useUpper = false;
+var useNumbers = false;
+var useSpecialChars = false;
 
 // →→→ Array to receive other arrays to generate password from ←←←
 var passwordAry = [];
@@ -69,15 +74,44 @@ var generatedPassword = "";
 
 // ↓↓↓ User input / prompts ↓↓↓
 
+// →→→ Getting desired password length ←←←
 var getPassLength = function () {
   var userPassLength = window.prompt(
     "Welcome to RandPass!\nPlease enter a number between:\n8-128"
   );
-  return (passLength = userPassLength);
+
+  if (userPassLength >= 8 && userPassLength <= 128) {
+    return (passLength = userPassLength);
+  } else {
+    window.alert("Must be between 8-128 characters long.");
+    getPassLength();
+  }
 };
 
+// →→→ Getting uppercase designation ←←←
+var getUpperUse = function () {
+  var userUpper = confirm(
+    "Would you like to use uppercase characters in your password??"
+  );
+  if (userUpper == true) {
+    passwordAry = passwordAry.concat(alphaBig);
+  }
+};
+
+var getNumberUse = function () {
+  var userNumber = confirm("Would you like to use numbers in your password?");
+  if (userNumber == true) {
+    passwordAry = passwordAry.concat(numList);
+  }
+};
+
+// ↓↓↓ Calling functions ↓↓↓
 getPassLength();
 console.log(passLength);
+getUpperUse();
+console.log(passwordAry);
+getNumberUse();
+console.log(passwordAry);
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
